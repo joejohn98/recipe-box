5f72eee860d6ae8368ebb1f01e69352b7a5f8134
joejohn98 <joejohn048@gmail.com>
2025-01-27 00:37:23 +0530

    wrapped app by themeProvider & recipeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
 import "./App.css";
+import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
+import { RecipeProvider } from "./context/RecipeContext";
+import { ThemeProvider } from "./context/ThemeContext";
+import { Toaster } from "react-hot-toast";
+
 
 function App() {
   return (
-    <>
-      
-    </>
+    <ThemeProvider>
+      <RecipeProvider>
+       <Router>
+        <div className="min-h-screen bg-gray-50 dark:bg-gray-900 transition-colors duration-200">
+         <main className="container mx-auto px-4 py-8">
+          <Routes>
+            <Route path="/" element={} />
+          </Routes>
+         </main>
+        </div>
+        <Toaster position="top-right"/>
+       </Router>
+      </RecipeProvider>
+    </ThemeProvider>
   );
 }
 
